86d3f61ec2dd7b548122ee204b3cae93e6650a5d
Dina <dinagiacobbi@gmail.com>
2024-03-11 08:39:42 -0800

    Adding ngModel binding, update html

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -10,10 +10,10 @@ import { Hero } from '../hero';
 export class HeroFormComponent {
 
 
-  powers = ['Really Smart', 'Super Flexible',
-            'Super Hot', 'Weather Changer'];
+  powers = ['Super Fast Binkies', 'High Jumps',
+            'Flops on Command', 'Super Cute'];
 
-  model = new Hero(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
+  model = new Hero(18, 'Peter Cottontail', this.powers[0], 'Bugs Bunny');
 
   submitted = false;
 
